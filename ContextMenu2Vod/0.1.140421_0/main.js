c997753cb3eaf3d5a9f5d42a02a30222a7f812d4
李怀奇 <never.be.evil.debian@gmail.com>
2014-04-22 18:43:09 +0800

    Change href vod code

--- ContextMenu2Vod/0.1.140421_0/main.js
+++ ContextMenu2Vod/0.1.140421_0/main.js
@@ -1,8 +1,15 @@
+vodHref = "http://vod.xunlei.com/share.html?&url={url}&from={from}";
 chrome.contextMenus.create({
     "title": "Context2VOD",
+    "contexts": ["selection"],
     "onclick": function(info, original_tab) {
+
+        vodHref=vodHref.replace("{url}", info.selectionText);
+        vodHref=vodHref.replace("{from}","vodHome");
+        
+        debugger;
         chrome.tabs.create({
-            "url": "http://www.google.com",
+            "url": vodHref,
             "openerTabId": original_tab.index + 1,
             "openerTabId": original_tab.id,
             "active": true
