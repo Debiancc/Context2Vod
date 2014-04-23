e42a6170fce2ad08f55c15252f1031e918256a23
李怀奇 <never.be.evil.debian@gmail.com>
2014-04-23 16:53:52 +0800

    commit vod interface js

--- ContextMenu2Vod/0.1.140421_0/main.js
+++ ContextMenu2Vod/0.1.140421_0/main.js
@@ -4,7 +4,7 @@ chrome.contextMenus.create({
     "contexts": ["selection"],
     "onclick": function(info, original_tab) {
 
-        vodHref=vodHref.replace("{url}", info.selectionText);
+        vodHref=vodHref.replace("{url}", encodeURI(info.selectionText));
         vodHref=vodHref.replace("{from}","vodHome");
         
         debugger;
@@ -16,3 +16,21 @@ chrome.contextMenus.create({
         });
     }
 });
+
+
+// ed2k://|file|The.Originals.S01E01.1080p.WEB-DL.DD5.1.H.264-KiNGS.mkv|
+// 1594486167|
+// 4D7A564B96135A17F9B4D43AE4D1C8F8|
+// h=XN2SEC5FZ373BSNL3HO4KJCIJTU4AMPL|/
+
+
+// ed2k://|file|The.Originals.S01E01.1080p.WEB-DL.DD5.1.H.264-KiNGS.mkv|
+// 1594486167|
+// 4D7A564B96135A17F9B4D43AE4D1C8F8|
+// h=XN2SEC5FZ373BSNL3HO4KJCIJTU4AMPL|/
+
+
+
+
+
+// http://vod.xunlei.com/share.html?&url=ed2k%3A%2F%2F%7Cfile%7CThe.Originals.S01E01.1080p.WEB-DL.DD5.1.H.264-KiNGS.mkv%7C1594486167%7C4D7A564B96135A17F9B4D43AE4D1C8F8%7Ch%3DXN2SEC5FZ373BSNL3HO4KJCIJTU4AMPL%7C%2F&from=un_5670
---
+++ ContextMenu2Vod/0.1.140421_0/vod-share.js
@@ -0,0 +1,100 @@
+(function() {
+    var b = {
+        style_1: "XL_CLOUD_VOD_BUTTON_yunyulan_btn",
+        style_2: "XL_CLOUD_VOD_BUTTON_duyan_btn",
+        style_3: "XL_CLOUD_VOD_BUTTON_yunyulan_btn",
+        style_4: "XL_CLOUD_VOD_BUTTON_duyan_btn"
+    };
+    if (typeof(window.TD_CLOUD_VOD_JS_EXIST) != "undefined") {
+        return
+    }
+    window.TD_CLOUD_VOD_JS_EXIST = true;
+    var e = function(f, g) {
+        if (f.readyState) {
+            f.onreadystatechange = function() {
+                if (f.readyState == "loaded" || f.readyState == "complete") {
+                    f.onreadystatechange = null;
+                    g && g()
+                }
+            }
+        } else {
+            f.onload = function() {
+                g()
+            }
+        }
+    };
+    var d = function(i) {
+        var m = i.attr("url") || "";
+        var f = i.attr("filename") || "";
+        var l = i.attr("from") || location.hostname;
+        var h = i.attr("filesize") || "";
+        var k = i.attr("cid") || "";
+        var j = i.attr("gcid") || "";
+        m = encodeURIComponent(m);
+        var g = ["http://vod.xunlei.com/share.html?", "&url=", m];
+        g = g.join("");
+        if (f) {
+            g += "&filename=" + encodeURIComponent(f)
+        }
+        if (h) {
+            g += "&filesize=" + encodeURIComponent(h)
+        }
+        if (l) {
+            g += "&from=" + encodeURIComponent(l)
+        }
+        if (k) {
+            k += "&cid=" + encodeURIComponent(k)
+        }
+        if (j) {
+            g += "&gcid=" + encodeURIComponent(j)
+        }
+        window.open(g, "TD_CLOUD_VOD")
+    };
+    var c = function() {
+        $.noConflict();
+        jQuery(document).ready(function(h) {
+            var i = document.createElement("link");
+            i.rel = "stylesheet";
+            i.type = "text/css";
+            i.href = "/stylesheets/share.css";
+            var f = document.getElementsByTagName("head")[0];
+            f.appendChild(i);
+            var g = h("a[name=TD_CLOUD_VOD_BUTTON]");
+            g.attr("title", "迅雷云预览");
+            g.addClass("XL_CLOUD_VOD_BUTTON_btn_b");
+            g.each(function(l) {
+                var k = h(this).attr("tclass") || "";
+                if (typeof(b[k]) == "undefined") {
+                    k = b.style_1
+                } else {
+                    k = b[k]
+                }
+                var m = h(this).attr("sclass");
+                if (!m) {
+                    m = "normal"
+                }
+                if (m == "big") {
+                    k += "_big"
+                } else {
+                    if (m == "small") {
+                        k += "_small"
+                    } else {
+                        k += "_normal"
+                    }
+                }
+                h(this).addClass(k);
+                var j = h(this).text() || "";
+                if (j) {}
+            });
+            g.live("click", function() {
+                var j = h(this).attr("td_vod_url");
+                d(h(this))
+            })
+        })
+    };
+    var a = document.createElement("script");
+    a.type = "text/javascript";
+    a.src = "http://vod.xunlei.com/library/jquery-1.7.2.min.js";
+    e(a, c);
+    document.body.appendChild(a)
+})();
